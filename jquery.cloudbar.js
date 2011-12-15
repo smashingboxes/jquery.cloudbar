7685a675e101f451b0d4b6f248be1df2146b4619
Nathan Hunzaker <nate.hunzaker@gmail.com>
2011-12-15 11:52:16 -0500

    Update jquery.cloudbar.js

--- jquery.cloudbar.js
+++ jquery.cloudbar.js
@@ -1,7 +1,7 @@
 // ------------------------------------- //
 // jQuery Cloudbar
 // ------------------------------------- //
-// Description: A simple sidebar and 
+// Description: A simple sidebar generator and 
 //              scroller plugin
 // Version: 0.0.1
 // Author: Nate Hunzaker
@@ -11,9 +11,11 @@
     
     var pluginName = 'cloudbar',
     defaults = {
-        taxonomy     : "h1",
-        padding      : 25,
-        stretch      : true
+        taxonomy          : "h2",
+        padding           : 25,
+        css               : "",
+        custom_labels     : [],
+        custom_list_items : ""
     };
 
 
@@ -31,12 +33,13 @@
 
     Plugin.prototype.scroll = function(e) {
         
-        var slot = $(e.target).attr("data-slot");
+        $("#cloudbar li").removeClass("selected");
         
-        var anim = {
-            // The offset of the element - the desired padding from the top
-            scrollTop: $(this.options.taxonomy).eq(slot).offset().top - this.options.padding
-        };
+        var slot = $(e.target).addClass("selected").attr("data-slot"),
+            anim = {
+                // The offset of the element - the desired padding from the top
+                scrollTop: $(this.options.taxonomy).eq(slot).offset().top - this.options.padding
+            };
 
 		    $('html, body').stop().animate(anim, this.options.speed);
     };
@@ -45,36 +48,71 @@
     Plugin.prototype.generateSidebar = function() {
         
         var fn       = this,
-            $sidebar = $("<ul id='cloudbar'></ul>");
-            
+            $sidebar = $("<div id='cloudbar'></div>").addClass(this.options.css),
+            $list    = $("<ul></ul>"),
+        
             template = function(props){
                 return $("<li data-slot='" + props.num + "'>" + props.text + "</li>");
             },
 
             hierarchy = this.options.taxonomy.split(" ");
         
+
         $(this.options.taxonomy).each(function(i) {
 
             var item = template({
                 num  : i,
-                text : $(this).text()
+                text : fn.options.custom_labels[i] || $(this).text()
             });
 
-            $sidebar.append(item);
+            $list.append(item);
         });
 
-
-        $sidebar.children("li").click(function(e) {
+        // Add click events (Before we add custom menu items, we don't
+        // want them triggering anything)
+        $list.children("li").click(function(e) {
             fn.scroll(e);
         });
 
-        $("body").append($sidebar);
+         // Add custom menu items
+        $list.append(this.options.custom_list_items);
+
+        // Select the first list item
+        $list.children("li").first().addClass("selected");
+        
+        // Add list items to sidebar
+        $sidebar.append($list);
+        
+        // Add sidebar to container
+        this.$element.append($sidebar);
 
     };
 
 
     Plugin.prototype.init = function () {
-        this.generateSidebar(); 
+
+        var fn = this;
+
+        // Add the sidebar
+        this.generateSidebar();
+        
+        // Autoselects sidebar items upon certain scroll levels
+        $(window).scroll(function(e) {
+            
+            // Only this function on every 10 pixels of scroll to
+            // improve performance
+            if ($(this).scrollTop() % 10 !== 0) { return false; }
+            
+            var $sidebar = $("#cloudbar"),
+                target  = 0;                  
+
+            $(fn.options.taxonomy).each(function(i) {
+                target = ( $(this).offset().top < ( $sidebar.offset().top + $sidebar.height())) ? i : target;
+            });
+
+            $("#cloudbar ul li").removeClass("selected").eq(target).addClass("selected");
+
+        });
     };
 
 
