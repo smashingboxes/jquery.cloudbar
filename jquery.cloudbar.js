a4cd39ce7a2fe90ec2725ed88efd6b9e71e3b605
Nathan Hunzaker <nate.hunzaker@gmail.com>
2011-12-16 11:31:19 -0500

    Update jquery.cloudbar.js

--- jquery.cloudbar.js
+++ jquery.cloudbar.js
@@ -3,7 +3,7 @@
 // ------------------------------------- //
 // Description: A simple sidebar generator and 
 //              scroller plugin
-// Version: 0.8.0
+// Version: 0.8.1
 // Author: Nate Hunzaker
 // ------------------------------------- //
 
@@ -24,7 +24,7 @@
         padding_initial     : 0,
         padding_forgiveness : 0.2,
 
-        navigation_arrows   : true        
+        navigation_arrows   : true
     };
 
     function Plugin( element, options ) {
@@ -64,7 +64,7 @@
     Plugin.prototype.generateSidebar = function() {
         
         var fn       = this,
-            $sidebar = $("<div id='cloudbar'></div>").addClass(this.options.css),
+            $sidebar = $("<div id='cloudbar'></div>").addClass(this.options.css).hide(),
             $list    = $("<ul></ul>"),
         
             template = function(props){
@@ -101,20 +101,27 @@
         // Add sidebar to container
         this.$element.append($sidebar);
 
+        this.sidebar_options = {
+            initial_margin: parseInt($("#cloudbar").css("margin-left"), 10)
+        };
+
+        $sidebar.delay(200).fadeIn(500);
+
     };
     
 
     Plugin.prototype.watchScroll = function() {
 
-        var fn = this;
-
+        var fn   = this,
+            func = arguments.callee;
+        
+        func.timeout = {};
+        
         // Autoselects sidebar items upon certain scroll levels
         $(window).scroll(function(e) {
-            
-            // Only this function on every 5 pixels of scroll to
-            // improve performance
-            if ($(this).scrollTop() % 10 !== 0) { return false; }
-            
+
+            var $this = $(window);
+
             var $sidebar = $("#cloudbar"),
                 target  = 0;                  
 
@@ -124,18 +131,23 @@
 
             fn.handleSelection(target);
             
-            var sidebarTop = ( $(window).scrollTop() > (fn.options.padding_initial * (1 - fn.options.padding_forgiveness))) ? fn.options.padding : fn.options.padding_initial;
+            var sidebarTop = ( $this.scrollTop() > (fn.options.padding_initial * (1 - fn.options.padding_forgiveness))) ? fn.options.padding : fn.options.padding_initial;
 
-            $("#cloudbar").animate({ top: sidebarTop }, 250);
+            var sidebarLeft = (fn.sidebar_options.initial_margin - $this.scrollLeft());
+            clearTimeout(func.timeout);
+
+            func.timeout = setTimeout(function() {
+                $("#cloudbar").animate({ top: sidebarTop, marginLeft: sidebarLeft }, 250);
+            }, 100);
+
         });
 
     };
-    
 
     Plugin.prototype.generateArrows = function() {
 
         var fn = this,
-            $arrow = $("<div class='cloudbar-arrow'></div>");
+            $arrow = "<div class='cloudbar-arrow'></div>";
      
         // Unfortunately, we need to get around JS's method of
         // scoping events
@@ -143,7 +155,7 @@
             fn.scroll.apply(fn, [e]);
         }                                          
 
-        $(this.options.taxonomy).each(function(i) {
+        $(this.options.taxonomy).not(":first").each(function(i) {
             $(this).before($arrow).prev(".cloudbar-arrow").attr("data-slot", i).click(scopedScroll);
         });
         
@@ -160,6 +172,9 @@
 
         // Generate arrow navigation if specified        
         this.options.navigation_arrows && this.generateArrows();
+
+        // Get things rolling
+        $(window).trigger("scroll");
        
     };
 
